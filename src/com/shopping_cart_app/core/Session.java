aefd666796cf581ba086e5b2d1e156824997960f
LeoTheLegion <mmena2017@fau.edu>
2019-10-28 22:30:43 -0400

    Created Session class. We will use cookies to sync each page.

--- src/com/shopping_cart_app/core/Session.java
+++ src/com/shopping_cart_app/core/Session.java
@@ -3,25 +3,19 @@ package com.shopping_cart_app.core;
 import java.util.HashMap;
 
 public class Session {
-	static Session currentSession;
-	HashMap<String,Object> cookies;
-	
-	
-	private Session() {
-		cookies = new HashMap<String,Object>();
-	}
+	static HashMap<String,Object> cookies;
 
 	public static void createNewSession() {
-		currentSession = new Session();
+		cookies = new HashMap<String,Object>();
 	}
 	
 	public static void createCookie(String cookieName, Object object) {
 		System.out.println("Creating Cookie: ("+ cookieName+ "|-|" + object.getClass()+")");
-		currentSession.cookies.put(cookieName, object);
+		cookies.put(cookieName, object);
 	}
 	
 	public static Object getCookie(String cookieName) {
-		Object obj = currentSession.cookies.get(cookieName);
+		Object obj = cookies.get(cookieName);
 		if(obj == null)
 			System.err.println("WARNING: The cookie("+cookieName +") was not found...");
 		return obj;
