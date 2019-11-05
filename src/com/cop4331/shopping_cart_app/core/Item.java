2ec4f1c33c2fc65e3d83a0c4bece2e2590f83c00
jament2016 <jament2016@fau.edu>
2019-11-05 10:07:46 -0500

    Testing sourcetree, Item and ItemDB classes created

---
+++ src/com/cop4331/shopping_cart_app/core/Item.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shopping_cart_app.src.com.cop4331.shopping_cart_app.core;
+
+/**
+ *
+ * @author Justin Ament
+ */
+public class Item {
+    String item_name;
+    String info;
+    int quantity;
+    
+    public Item() {
+        
+    }
+}
